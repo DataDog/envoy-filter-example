9f8ed956d3c63361a90e2141b77250751c3fcb57
Ubuntu <ubuntu@ip-10-128-172-195.ec2.internal>
2023-07-13 14:05:13 +0000

    removed unneeded code
    Signed-off-by: Ubuntu <ubuntu@ip-10-128-172-195.ec2.internal>

--- header-rewrite-filter/header_rewrite_integration_test.cc
+++ header-rewrite-filter/header_rewrite_integration_test.cc
@@ -9,16 +9,10 @@ public:
   /**
    * Initializer for an individual integration test.
    */
-  void SetUp() override { }
+  void SetUp() override {}
   void SetUp(std::string config) { initialize(config); }
 
-  void initialize() override {
-    config_helper_.prependFilter(
-        "{ name: sample, typed_config: { \"@type\": type.googleapis.com/sample.Decoder, key: via, "
-        "val: sample-filter } }");
-    HttpIntegrationTest::initialize();
-  }
-
+  void initialize() override {}
   void initialize(std::string config) {
     config_helper_.prependFilter(config);
     HttpIntegrationTest::initialize();
