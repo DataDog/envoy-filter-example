805bba5d8348183f01ff13de1e6293e0879f1a4f
Ubuntu <ubuntu@ip-10-128-172-195.ec2.internal>
2023-07-26 14:51:09 +0000

    added append header operation
    Signed-off-by: Ubuntu <ubuntu@ip-10-128-172-195.ec2.internal>

--- header-rewrite-filter/header_processor.cc
+++ header-rewrite-filter/header_processor.cc
@@ -129,7 +129,7 @@ namespace HeaderRewriteFilter {
 
 
     absl::Status AppendHeaderProcessor::executeOperation(Http::RequestOrResponseHeaderMap& headers, SetBoolProcessorMapSharedPtr bool_processors) {
-        const std::pair<absl::Status, bool> condition_result = evaluateCondition(bool_processors);
+        const std::tuple<absl::Status, bool> condition_result = evaluateCondition(bool_processors);
         const absl::Status status = std::get<0>(condition_result);
         if (status != absl::OkStatus()) {
             return status;
